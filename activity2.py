258503bb7724fe12489a1a18ac96dd90c3f18397
peeyush2025 <sudhakarln82@gmail.com>
2025-11-02 19:34:02 +0530

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,7 @@
+class vehicle:
+    def __init__(self, max_speed, milage):
+        self.max_speed = max_speed
+        self.milage = milage
+modelx=vehicle(240,18)
+print("model max speed :",modelx.max_speed)
+print("model milage :",modelx.milage)
