be1f911da66893bf24eb69dde0edad5c7314498d
peeyush2025 <sudhakarln82@gmail.com>
2025-11-22 19:30:04 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,5 +1,21 @@
-class student:
-    grade=10
-    print("Hi I am a student of grade",grade)
-
-ob=student()
+class A:
+    def __init__(self,a):
+        self.a=a
+    def __it__(self,other):
+        if (self.a<other.a):
+            return "ob1 is less than ob2"
+        else:
+            return "ob2 is less than ob1"
+    def __eq__(self,other):
+        if (self.a==other.a):
+            return " both are equal"
+        else:
+            return " both are not equal"
+ob1=A(2)
+ob2=A(3)
+print("passed values:",ob1.a,"and",ob2.a)
+print(ob1 < ob2)
+ob3=A(4)
+ob4=A(4)
+print("passed values:",ob3.a,"and",ob4.a)
+print(ob3 == ob4)
